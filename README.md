f6e34dd12a2dd90ccb6bf4867156d55f40152f71
Wemerson Jesus <wejesuss@gmail.com>
2020-09-14 21:16:44 -0300

    feat: press left/right arrow key to change chapters

---
+++ README.md
@@ -0,0 +1,21 @@
+# novel-experience
+
+<h3>
+    This is an extension for my personal use.  
+</h3>
+
+Exclusively for the [King Of Gods](https://wuxiaworld.online/king-of-gods/) and [Against The Gods](https://www.wuxiaworld.com/novel/against-the-gods/) (Wuxiaworld without a user account)
+
+<p>If you want to use too, do this:</p>
+
+- Download it:
+    ```bash
+    git clone https://github.com/wejesuss/novel-experience.git
+    ```
+
+- Open Google Chrome, go to extensions page - probably (chrome://extensions/)
+
+- You have to enable the 'Developer mode', after this click the 'load unpacked' button and choose the foolder you just have donwload yet.
+
+- Enjoy ☺️
+
---
+++ content.js
@@ -0,0 +1,63 @@
+function scroll(x, y) {
+    document.documentElement.scroll({behavior: "smooth", top: y})
+}
+
+setTimeout(() => {
+    const url = window.location.href
+
+    // remove protection ATG
+    if(url.includes('against-the-gods')) {
+        document.body.style.overflow = 'initial'
+        document.querySelectorAll('.modal-backdrop, .fade.in').forEach(element => element.remove())
+        document.querySelectorAll('.text-disabled').forEach(element => {
+            element.classList.remove('text-disabled')
+        })
+
+        setNextPrevChapter()
+    }
+    
+    // add identation to KOG
+    const contentArea = document.querySelector('div.content-area') || document.querySelector('div.cha-words')
+    if(url.includes('king-of-gods') && contentArea) {
+        const allPTags = contentArea.querySelectorAll('p')
+
+        allPTags.forEach(p => {
+            const div = document.createElement('div')
+            const br = document.createElement('br')
+            const newP = p.cloneNode(true)
+            
+            div.append(newP, br)
+            
+            p.replaceWith(div)
+        })
+    }
+
+    alert('divirta-se')
+
+    const id = setInterval(() => {
+        scroll(0, document.documentElement.scrollTop)
+        
+        document.documentElement.scrollTop += 2
+    }, 200)
+    console.log("Interval id:" + id, 'Type clearInterval(id) to stop scroll')
+
+}, 2000)
+
+function setNextPrevChapter() {
+    window.addEventListener('keydown', (e) => {
+        const currentNovel = e.view.location.pathname
+        const currentChapterPosition = currentNovel.lastIndexOf('-')
+        const currentChapter = Number(currentNovel.slice(currentChapterPosition + 1))
+
+        const nextChapter = currentChapter + 1
+        const previousChapter = currentChapter - 1
+
+        if(e.key === "ArrowRight") {
+            location = currentNovel.replace(currentChapter, nextChapter)
+        }
+
+        if(e.key === "ArrowLeft") {
+            location = currentNovel.replace(currentChapter, previousChapter)
+        }
+    })
+}
